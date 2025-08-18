e528458d79ac9c225f49418eae74f73230b6f61f
Arjun Nyaupane <arjunnyaupane16@gmail.com>
2025-08-18 22:48:13 +0530

    your message

--- app/components/OrderCard.js
+++ app/components/OrderCard.js
@@ -18,7 +18,7 @@ import {
   View,
   findNodeHandle
 } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { MaterialIcons as Icon } from '@expo/vector-icons';
 import styles from '../styles/OrdersStyles';
 import { deleteOrder, markOrderAsPaid, updateOrderStatus } from '../utils/orderApi';
 
